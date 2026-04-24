36f91cb9c460557118bff8df5ae15f06c606c941
Zhaseeek <zseri1@stu.vistula.edu.pl>
2026-04-24 12:21:31 +0200

    add search

--- app.py
+++ app.py
@@ -1,10 +1,26 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route('/')
+recipes = [
+    {"name": "Chicken Soup", "ingredients": "chicken water salt"},
+    {"name": "Pasta", "ingredients": "pasta tomato cheese"},
+    {"name": "Salad", "ingredients": "tomato cucumber lettuce"},
+    {"name": "Omelette", "ingredients": "egg milk salt"},
+]
+
+@app.route('/', methods=['GET', 'POST'])
 def home():
-    return render_template('index.html')
+    results = []
+
+    if request.method == 'POST':
+        user_input = request.form['ingredients'].lower()
+
+        for recipe in recipes:
+            if user_input in recipe["ingredients"]:
+                results.append(recipe["name"])
+
+    return render_template('index.html', results=results)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=10000)
